acac4f5e278c2d7f96b48ccf2a611cb29da4cc12
ElTomyy <tomasirioyen@gmail.com>
2023-07-08 17:00:51 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+let nombre = prompt("Escribe tu nombre: ")
+
+function hola(){
+    console.log("Hola " + nombre)
+}
+
+function suma(num1,  num2){
+    let resultado = num1 + num2
+    console.log(resultado)
+}
+
+hola()
+suma(1, 5)
+
+
