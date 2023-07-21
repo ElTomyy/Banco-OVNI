7af54d6573ead1d3f740a4f7a0e90cf25cd07fe5
ElTomyy <tomasirioyen@gmail.com>
2023-07-21 15:56:36 -0300

    Update object function

--- index.js
+++ index.js
@@ -1,64 +1,81 @@
 //simulador de retiros y depositos virtuales
+class cuentas {
+    constructor(nombre, apellido, numTarjeta, password) {
+        this.nombre = nombre
+        this.apellido = apellido
+        this.numTarjeta = numTarjeta
+        this.password = password
+        this.saldo = 0
+        this.retiro = [0]
+        this.deposito = [0]
+    }
+}
 
-let usuario = prompt("Ingrese su nombre de usuario: ")
-let tarjeta = "233409133245"
-let codigo = "546"
-let saldo = 50000
-let retiro = 0
-let deposito = 0
+function isNumbre(variable){
+    while(isNaN(variable)){
+        variable = parseInt(prompt(`El dato ingresado no esta permitido, ingrese un numero (saldo $${cuenta1.saldo})`))
+    }
+    return variable
+}
+
+function getTarjeta() {
+    verificacionTarjeta = parseInt(prompt("Ingrese su numero de tarjeta de credito "))
+    while(isNaN(verificacionTarjeta)){
+        verificacionTarjeta = parseInt(prompt("El dato ingresado no corresponde a un numero de tarjeta, intente devuelta"))
+    }
+    return verificacionTarjeta
+}
+
+function getPassword() {
+    verificacionPassword = parseInt(prompt("Ingese su codiogo de seguridad "))
+    while(isNaN(verificacionPassword) || verificacionPassword > 999){
+        verificacionPassword = parseInt(prompt("El codigo ingresado supera los 3 digitos y/o no corresponde a un codigo numerico"))
+    }
+    return verificacionPassword
+}
+
+let nombre = prompt("Ingrese su nombre")
+let apellido = prompt("Ingrese su apellido")
+let numDeTarjeta = getTarjeta()
+let password = getPassword()
 let exit = "si"
 
-function numTarjeta(){
-    verificacionTarjeta = prompt("Ingrese su numero de tarjeta de credito ")
-}
+const cuenta1 = new cuentas(nombre, apellido, numDeTarjeta, password)
 
-function numCodigo(){
-    verificacionCodigo = prompt("Ingese su codiogo de seguridad ")
-}
+while (exit == "si") {
 
-alert("Genial " + usuario + ", su tarjeta de credito es: " + tarjeta + "\n y su codigo de seguridad es: " + codigo)
+    let solicitud = prompt(`Usted tiene un saldo de $${cuenta1.saldo}, le gustaria hacer un deposito o un retiro ?`).toLowerCase()
 
-numTarjeta()
-numCodigo()
-
-while(verificacionTarjeta != "233409133245" || verificacionCodigo != "546"){
-    alert("Los datos ingresados no coinciden, pruebe devuelta a continuacion")
-    numTarjeta()
-    numCodigo()
-}
-
-while(exit == "si"){
-
-let solicitud = prompt("Usted tiene un saldo de $" + saldo + ", le gustaria hacer un deposito o un retiro ?")
-solicitud = solicitud.toLowerCase()
-
-while(solicitud != "deposito" && solicitud != "retiro"){
-    alert("La opcion seleccionada no existe, intente devuelta")
-    solicitud = prompt("Usted tiene un saldo de $" + saldo + ", le gustaria hacer un deposito o un retiro ?")
-}
-
-if(solicitud == "retiro"){
-    retiro = parseInt(prompt("Cuanto le gustaria retirar (saldo &" + saldo + ") ?"))
-    while(retiro > 50000){
-        alert("El retiro excede los fondos en la cuenta, intete devuelta")
-        retiro = parseInt(prompt("Cuanto le gustaria retirar (saldo &" + saldo + ") ?"))
+    while (solicitud != "deposito" && solicitud != "retiro") {
+        alert("La opcion seleccionada no existe, intente devuelta")
+        solicitud = prompt(`Usted tiene un saldo de $${cuenta1.saldo}, le gustaria hacer un deposito o un retiro ?`).toLowerCase()
     }
-    saldo -= retiro
-    alert("El retiro se cumplio con exito, su saldo actual es de $" + saldo)
-}
-else if(solicitud == "deposito"){
-    deposito = parseInt(prompt("Ingrese el monto que desea depositar (saldo $" + saldo + ")"))
-    saldo += deposito
-    alert("El deposito se cumplio con exito, su saldo actual es de $" + saldo)
+
+    if (solicitud == "retiro") {
+        cuenta1.retiro.unshift(isNumbre(parseInt(prompt(`Cuanto le gustaria retirar (saldo $${cuenta1.saldo})?`))))
+        while (cuenta1.retiro[0] > cuenta1.saldo) {
+            cuenta1.retiro.shift()
+            alert("El retiro excede los fondos en la cuenta, intete devuelta")
+            cuenta1.retiro.unshift(isNumbre(parseInt(prompt(`Cuanto le gustaria retirar (saldo $${cuenta1.saldo})?`))))
+        }
+        cuenta1.saldo -= cuenta1.retiro[0]
+        alert(`El retiro se cumplio con exito, su saldo actual es de $${cuenta1.saldo}`)
+    }
+    else if (solicitud == "deposito") {
+        cuenta1.deposito.unshift(isNumbre(parseInt(prompt(`Ingrese el monto que desea depositar (saldo $${cuenta1.saldo})`))))
+        cuenta1.saldo += cuenta1.deposito[0]
+        alert(`El deposito se cumplio con exito, su saldo actual es de $${cuenta1.saldo}`)
+    }
+
+    exit = prompt("Le gutaria hacer algun otro tipo de deposito o retiro ? (si, no)").toLowerCase()
+
+    while (exit != "si" && exit != "no") {
+        alert("La opcion seleccionada no existe, intente devuelta")
+        exit = prompt("Le gutaria hacer algun otro tipo de deposito o retiro ? (si, no)").toLowerCase()
+    }
 }
 
-exit = prompt("Le gutaria hacer algun otro tipo de deposito o retiro ? (si, no)")
-exit = exit.toLowerCase()
-
-while(exit != "si" && exit != "no"){
-    alert("La opcicon seleccionada no existe, intente devuelta")
-    exit = prompt("Le gutaria hacer algun otro tipo de deposito o retiro ? (si, no)")
-}
-}
+console.log(`Este fue su registro de depositos en el dia de hoy ${cuenta1.deposito}`)
+console.log(`Este fue su registro de retiros en el dia de hoy ${cuenta1.retiro}`)
 
 
